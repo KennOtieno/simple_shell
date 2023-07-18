a790fa2abdaad35d43898b18e24a9c00c1553579
KennOtieno <otiken001@gmail.com>
2023-07-18 03:15:48 -0700

    _atoi.c

---
+++ _atoi.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * interactive - interactive mode
+ * @info: the address
+ *
+ * Return: none
+ */
+int interactive(info_t *info)
+{
+	return (isatty(STDIN_FILENO) && info->readfd <= 2);
+}
+
+/**
+ * is_delim - check char if is delimeter
+ * @c: check char
+ * @delim: string
+ * Return: none
+ */
+int is_delim(char c, char *delim)
+{
+	while (*delim)
+		if (*delim++ == c)
+			return (1);
+	return (0);
+}
+
+/**
+ * _isalpha - alphabet char
+ * @c: char
+ *
+ * Return: none
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
+/**
+ * _atoi - change string to integer
+ * @s: string to be change
+ *
+ * Return: none
+ */
+
+int _atoi(char *s)
+{
+	int a, sign = 1, flag = 0, output;
+	unsigned int result = 0;
+
+	for (a = 0; s[a] != '\0' && flag != 2; a++)
+	{
+		if (s[a] == '-')
+			sign *= -1;
+
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			flag = 1;
+			result *= 10;
+			result += (s[a] - '0');
+		}
+		else if (flag == 1)
+			flag = 2;
+	}
+
+	if (sign == -1)
+		output = -result;
+	else
+		output = result;
+
+	return (output);
+}
