fa7241652d92b90a3ca5660c3e1ddf0a21cec938
Darren Harris <theharriscode401@gmail.com>
2023-07-20 06:12:25 -0700

    main.c finished

--- main.c
+++ main.c
@@ -1,62 +1,44 @@
-#include "shell.h"
+#inlcude "shell.h"
+
 /**
- * main - the simple shell
- * argc - integer count of arguments
- * argv - double pointer for command line arguments
- * Return - 0
+ * main - the main entry point
+ * @ac: argument count
+ * @av: argument vector (array)
+ * Return: 0 (Success) 1 (Failure)
  */
-
-int main(int argc, char **argv)
+int main(int ac, char **av)
 {
-	char *prompt = "simpleshell$ ";
-	char *lineptr = NULL, *lineptr_cpy = NULL;
-	size_t n = 0;
-	ssize_t exit_char_read = 0;
-	char *delim = " \n";
-	char *token = NULL;
-	int token_count = 0;
-	int i;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	(void)argc; (void)argv;
-	while(1) /*shell loop*/
+	/*inline assembly code*/
+	asm ("mov %1, %0\n\t"
+	     "add $3, %0"
+	     : "=r" (fd),
+	     : "r" (fd));
+
+	if (ac == 2)
 	{
-		printf("%s", prompt);
-		exit_char_read = getline(&lineptr, &n, stdin); /*user input*/
-		/*checks for EOF character or if getline function fails*/
-		if (exit_char_read == -1)
-			return (-1);
-		lineptr_cpy = malloc(sizeof(lineptr) * exit_char_read);
-		if (lineptr_cpy == NULL)
+		fd = open(av[1], O_READONLY);
+		if (fd == -1)
 		{
-			perror("Memory Allocation Error");
-			return(-1);
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
+			{
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
+			}
+			return (EXIT_FAILURE);
 		}
-		/*copying string*/
-		strcpy(lineptr_cpy, lineptr);
-		/*parsing through string*/
-		token = strtok(lineptr, delim);
-		while (token != NULL)
-		{
-			token_count++; /* counts from 1*/
-			token = strtok(NULL, delim);
-		}
-		token_count++;
-		argv = malloc(sizeof(char *) * token_count);
-		token = strtok(lineptr_cpy, delim);
-	        for (i = 0; token != NULL; i++)
-		{
-			argv[i] = malloc(sizeof(char) * strlen(token));
-			strcpy(argv[i], token);
-			token = strtok(NULL, delim);
-		}
-		argv[i] = NULL;
-		int counter;
-		for (counter = 0; counter < token_count -1; ++counter)
-			printf("%s\n", argv[counter]);
-		printf("%s\n", lineptr);
+		info->readfd = fd;
 	}
-	free(argv);
-	free(lineptr);
-	free(lineptr_cpy);
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
