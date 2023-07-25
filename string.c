525629e5e75b342f73b56c68fe03129746f9934e
KennOtieno <otiken001@gmail.com>
2023-07-25 03:09:28 -0700

    string.c

---
+++ string.c
@@ -0,0 +1,75 @@
+#include "shell.h"
+
+/**
+ * _strlen - return string length
+ * @s: string length
+ *
+ * Return: none
+ */
+int _strlen(char *s)
+{
+	int a = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		a++;
+	return (a);
+}
+
+/**
+ * _strcmp - strings lexicogarphic comparison
+ * @s1: string one
+ * @s2: string two
+ *
+ * Return: none
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - check if the needle begin with haystack
+ * @haystack: search the str
+ * @needle: find substring
+ *
+ * Return: none
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - two strings
+ * @dest: buffer
+ * @src: buffer source
+ *
+ * Return: pointer buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
