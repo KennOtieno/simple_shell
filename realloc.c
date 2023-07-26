343ea659770a2e5c4ab2b62cb847701960377400
KennOtieno <otiken001@gmail.com>
2023-07-26 07:26:17 -0700

    realloc

---
+++ realloc.c
@@ -0,0 +1,65 @@
+#include "shell.h"
+
+/**
+ * _memset - fill memory with constant byte
+ * @s: pointer to memory area
+ * @n: bytes amount
+ * @b: bytes to fill *s
+ *
+ * Return: none
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int a;
+
+	for (a = 0; a < n; a++)
+		s[a] = b;
+	return (s);
+}
+
+/**
+ * ffree - free strings
+ * @pp: strings
+ *
+ * Return: none
+ */
+void ffree(char **pp)
+{
+	char **a = pp;
+
+	if (!pp)
+		return;
+	while (*pp)
+		free(*pp++);
+	free(a);
+}
+
+/**
+ * _realloc - reallocates memory block
+ * @ptr: pointer to previous malloc'ated block
+ * @new_size: size byte of the new block
+ * @old_size: size byte of the o block
+ *
+ * Return: none
+ */
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
+{
+	char *p;
+
+	if (!ptr)
+		return (malloc(new_size));
+	if (!new_size)
+		return (free(ptr), NULL);
+	if (new_size == old_size)
+		return (ptr);
+
+	p = malloc(new_size);
+	if (!p)
+		return (NULL);
+
+	old_size = old_size < new_size ? old_size : new_size;
+	while (old_size--)
+		p[old_size] = ((char *)ptr)[old_size];
+	free(ptr);
+	return (p);
+}
